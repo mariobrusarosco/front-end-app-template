0f9449e76785b8e6d91222049a7e323e7818aa3b
Mario Brusarosco <mariobrusarosco@hotmail.com>
2021-08-18 00:25:55 -0300

    fixes: Templates + dead code removal

---
+++ playground/src/domains/equity/context/useSimulation.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import useSimulation from "@domains/equity/components/useSimulation"
+
+
+describe("when rendering",() => {
+  it("lorem ipsum", () => {
+    expect(false).toBe(true)
+  })
+})
+
