29291df4241f927352a3faf1cceebda3a8f0797d
Mario Brusarosco <mariobrusarosco@hotmail.com>
2021-08-18 21:36:55 -0300

    fix: adding inquirer

--- packages/beer/src/questions/index.ts
+++ packages/beer/src/questions/index.ts
@@ -1,11 +1,9 @@
-import { projectName } from "./projectName";
 import { currentDomains } from "./currentDomains";
 import { reactElementType } from "./reactElementType";
 import { reactElementName } from "./reactElementName";
 import { newDomainName } from "./newDomainName";
 
 export default {
-  askAboutProjectName: projectName,
   askAboutNewDomainName: newDomainName,
   askAboutCurrentDomains: currentDomains,
   askAboutReactElementType: reactElementType,
