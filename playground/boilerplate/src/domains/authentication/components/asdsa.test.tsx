e09bd8db03cc1018a0796ed5cda8b69ee54d2562
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-07-08 19:48:15 -0300

    feat: adding more content for a playground project

---
+++ playground/boilerplate/src/domains/authentication/components/asdsa.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import } from "@domains//components/asdsa"
+
+
+describe("when rendering",() => {
+  it("lorem ipsum", () => {
+    expect(false).toBe(true)
+  })
+})
+
---
+++ playground/boilerplate/src/domains/authentication/components/asdsa.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+  lorem: string
+}
+
+const asdsa: React.FunctionComponent<Props> = ({
+  lorem
+}) => {
+  return <div>{lorem}</div>
+}
+
+export default asdsa
